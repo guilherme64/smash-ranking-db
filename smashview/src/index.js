c35aa2b01b1d07489888bc9a6c8483c095f3aa60
Guilherme <guilhermemelo6@outlook.com>
2018-10-25 17:34:30 -0300

    Dev server is now running

--- smashview/src/index.js
+++ smashview/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends Component{
   render(){
     return(
-      <h1>Smash Ranking DB is (almost) ALIVE!</h1>
+      <h1>Smash Ranking DB is ALIVE!</h1>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const outputDirectory = "dist";
+const NodemonPlugin = require('nodemon-webpack-plugin')
 module.exports = {
     entry: "./smashview/src/index.js",
     output: {
@@ -10,30 +11,23 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.js$/,
-                include: /smashview\/\src/,
-                exclude: [/node_modules/, /smash/],
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+            test: /\.js$/,
+            include: __dirname + "/smashview\/\src/",
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+
+        }]
     },
     devServer: {
         port: 3000,
         open: true,
-        proxy: {
-            "/api": "http://localhost:8080"
-        }
     },
     plugins: [
-        new CleanWebpackPlugin([outputDirectory]),
+        new CleanWebpackPlugin(['/smashview/' + outputDirectory]),
         new HtmlWebpackPlugin({
-            template: "./smashview/public/index.html"
-        })
+            template: "./smashview/public/index.html",
+            filename: "./index.html"
+        }),
+        new NodemonPlugin()
     ]
 };
